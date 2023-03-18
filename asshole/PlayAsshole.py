51bdc71bf3701d06f587cc374a5ebc3f289a842c
Richard Kilgour <richard.kilgour@gmail.com>
2023-03-18 21:54:17 +0100

    Load players from file

--- asshole/PlayAsshole.py
+++ asshole/PlayAsshole.py
@@ -5,6 +5,9 @@ Play a game of Asshole locally
 Creates a GM and some players, and lets the GM control the game
 """
 import logging
+import yaml
+import sys
+
 
 from asshole.GameMaster import GameMaster
 from asshole.player.HumanPlayer import HumanPlayer
@@ -16,14 +19,15 @@ from asshole.player.PlayerSplitter import PlayerSplitter
 def main():
     # logging.basicConfig(handlers=[logging.FileHandler('test.log', 'w', 'utf-8')], level=logging.DEBUG)
     logging.basicConfig(handlers=[logging.FileHandler('test.log', 'w', 'utf-8')], level=logging.NOTSET)
+
     gm = GameMaster()
-    # Players can be player_simple, player_splitter, player_holder, human_player
-    gm.make_player(PlayerHolder, "Richard")
-    # gm.make_player(PlayerSplitter, "Silvia")
-    gm.make_player(HumanPlayer, "Silvia")
-    gm.make_player(PlayerSplitter, "Sara")
-    gm.make_player(PlayerSimple, "snAkbar")
-    gm.play(number_of_rounds=1000)
+
+    config = yaml.safe_load(open("./config.yaml"))
+    players = [config['player1'], config['player2'], config['player3'], config['player4'], ]
+    for i, p in enumerate(players):
+        player_class = getattr(sys.modules[__name__], p['type'])
+        gm.make_player(player_class, p['name'])
+    gm.play(number_of_rounds=100)
 
 
 if __name__ == '__main__':
