7835ef1eea4912ef1354e012c847be84139617da
Richard (Laptop) <richardkilgour@gmail.com>
2025-03-04 15:05:02 +0100

    Even simple players should not keep double jokers or sets of 2s

--- asshole/players/PlayerSimple.py
+++ asshole/players/PlayerSimple.py
@@ -18,15 +18,15 @@ class PlayerSimple(AbstractPlayer):
         Return a list of cards, or None if the desire is to pass
         """
         super().play()
-        # We know the target meld, and play the lowest option
+        # We know the target meld, and play the lowest option that beats the meld
         possible_plays = self.possible_plays(self.target_meld)
         # If there is the only remaining option (usually 'pass'), take it
         if len(possible_plays) == 1:
             return possible_plays[0]
 
         for s in possible_plays[:-1]:
-            # If this is not a set, just play it (use will_split()?)
-            if self.number_of_cards_of_value(s.cards[0].get_value()) == len(s.cards):
+            # Do not split up a set (except 2s and Jokers)
+            if s.cards[0].get_value() > 11 or self.number_of_cards_of_value(s.cards[0].get_value()) == len(s.cards):
                 return s
             logging.info(f'Found a set of {len(s.cards)} x {s.cards[0]}, so not playing {s.cards}')
 
