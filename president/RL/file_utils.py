624f9b2b4784e2601efce6f00aa0b05452ec509d
Richard Kilgour <richardkilgour@gmail.com>
2025-03-30 22:21:46 +0200

    Moved load function to the model file

--- president/RL/file_utils.py
+++ president/RL/file_utils.py
@@ -1,8 +1,5 @@
 import torch
 
-from president.RL.simple_model import SimpleModel
-
-
 def save_expert_data(inputs, targets, filename="expert_data.pt"):
     """Converts lists of tensors to single tensor and saves to file."""
     torch.save({"inputs": inputs, "targets": targets}, filename)
@@ -11,13 +8,6 @@ def save_expert_data(inputs, targets, filename="expert_data.pt"):
 def load_expert_data(filename="expert_data.pt"):
     data = torch.load(filename)
     return data["inputs"], data["targets"]
-
-
-def load_model(filepath="best_model.pt"):
-    model = SimpleModel()
-    model.load_state_dict(torch.load(filepath))
-    model.eval()  # Set to evaluation mode
-    return model
 
 def main():
     # Load the data file and dump it to console
